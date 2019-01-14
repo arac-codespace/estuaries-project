cbfba74aeed9c407df7593e5815be0a0b8a11c57
arac-codespace <alex.aguirre0026@gmail.com>
2019-01-14 18:05:28 -0400

    Updating readme, rewrote the get data function...

--- Readme.md
+++ Readme.md
@@ -1,20 +1,30 @@
 # Jobos Bay dataset exploration
-
 Finally back to coding!
-
----
 This repository is meant to be a "playground" for me to explore the data available for the Jobos Bay.
 
 ---
-Data
+### Data
+This repository is based on NERRS data:
+http://cdmo.baruch.sc.edu/
+
+Some notes about the NERRS dataset:
+I found some issues while playing around with the data.  First, the csv files have whitespace that may result in unexpected issues when querying/inserting data to and from a dataframe or database.  It may also complicate things when trying to load the csv file using certain libraries.  Another interesting issue is that fields with no values are flagged as historical (<4>) instead of missing value (<-2>).
+
+Another issue is that -at least in the meteorological data- there are '-99' values that are not taken into account by the flag values.  It's therefore necessary to filter those values out when working with the data.  Readme has the following to say about those '-99' values...
+
+"In June 2009, in order to repopulate data tables, the Centralized Data Management Office removed all -99999 from SWMP weather data files and replaced them with -99."
+
+---
+
+### Other sources of data for future reference...
 https://products.coastalscience.noaa.gov/collections/ltmonitoring/nsandt/data2.aspx
 http://cdmo.baruch.sc.edu/dges/
 CEAP, USGS and CariCOOS stations.
-
 ---
-Potential research topics:
+### Potential research topics:
 http://drna.pr.gov/wp-content/uploads/2017/08/Final-2017-2022-Jobos-Bay-Management-Plan.pdf
 "Develop  field  maps  to  locate  aquifer  points  of  discharge  in  collaboration  with the Research Advisory Committee. 
+
 The Research Advisory Committee recommended initiating this effort by identifying the points  where  changes  in  salinity  have  been  observed,  and  analyzing  historical events that may explain those changes. To date, a reduction in salinity has been observed at  stations 19 and 20. This could indicate seepage, and should be further monitored."
 
-Paper also mentions: Development of an hydrodynamic model.  Development of a groundwater flow model.  Research on carbon proccesses for climate change considerations.
+Paper also mentions: Development of an hydrodynamic model, development of a groundwater flow model and research on carbon proccesses for climate change considerations.
--- data/create_visualization.py
+++ data/create_visualization.py
@@ -7,42 +7,76 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, MetaData, ForeignKey
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.dialects.postgresql import DOUBLE_PRECISION, TIMESTAMP
+import pdb
+
+
+def table_params(table_name):
+
+    quality_params = [
+        "temp", "spcond", "sal", "do_pct",
+        "do_mgl", "depth", "ph", "turb"
+    ]
+
+    nutrient_params = [
+        "po4f", "nh4f", "no2f", "no3f",
+        "no23f", "chla_n",
+    ]
+    '''
+        Excluding bc not "aggregatable"
+        MaxWSpdT, Wdir, SDWDir
+    '''
+    meteorological_params = [
+        "atemp", "rh", "bp", "wspd", "maxwspd",
+        "totpar", "totprcp", "totsorad"
+    ]
+
+    switcher = {
+        "water_quality": {"params": quality_params},
+        "water_nutrient": {"params": nutrient_params},
+        "meteorology": {"params": meteorological_params},
+    }
+
+    table_params = switcher.get(table_name)
+
+    return table_params
 
 
 def create_math_sql(fx, params):
     fx_statements = []
-    for param in params:
-        fx_statements.append(
-            f"{fx}(case when f_{param} not like '%%-%%' then {param} end) as {fx}_{param}"
-        )
+    for param in params['params']:
+        print(param)
+        '''
+          Had to include this condition bc I found invalid values (-99)
+          for atemp in meteorological data...
+          Apparently, NERRS replaced -99999 to -99, so I'll check for -99
+          for every param...
+        '''
+        sql = f"{fx}(case when (f_{param} not like '%%-%%') and ({param} != -99) and ({param} != -99999) then {param} end) as {fx}_{param}"
+
+        fx_statements.append(sql)
     # Create expression string
     expression = ",".join(fx_statements)
     print(expression)
     return expression
 
 
-def get_water_nutrients():
+def get_aggregate_by_date(table_name):
 
-    nutrient_params = [
-        "PO4F", "NH4F", "NO2F", "NO3F",
-        "NO23F", "CHLA_N",
-    ]
+    params = table_params(table_name)
 
-    column_sql = create_math_sql("avg", nutrient_params)
+    column_sql = create_math_sql("avg", params)
 
     sql = f"""
         SELECT
             date_trunc('month',datetimestamp),
             stationcode,
             {column_sql}
-        FROM water_nutrient
+        FROM {table_name}
         GROUP BY date_trunc('month',datetimestamp), stationcode
         ORDER BY date_trunc
     """
     uri = create_cfg_uri()
     engine = create_engine(uri)
-    # sql = "SELECT * FROM water_nutrient"
-
     df = pd.read_sql(sql, con=engine)
 
     return df
